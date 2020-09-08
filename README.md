7b395bb8c286e138e56b5bf4634a41349f3a3196
CJBuchel <connorbuchel@gmail.com>
2020-09-08 17:24:19 +0800

    push

---
+++ README.md
@@ -0,0 +1,3 @@
+# Linux setup
+
+### run: sudo wget -qO - https://github.com/CJBuchel/linux-setup/blob/master/linux-setup.sh?raw=1 | bash
---
+++ linux-setup.sh
@@ -0,0 +1,21 @@
+#!/bin.sh
+
+sudo apt-get update
+
+# packages
+sudo apt-get install -y avahi-daemon libnss-mdns v4l-utils imagemagick
+sudo apt-get install -y build-essential cmake cmake-curses-gui pkg-config
+sudo apt-get install -y libjpeg-dev libtiff5-dev libjasper-dev libavcodec-dev libavformat-dev libswscale-dev libeigen3-dev libxvidcore-dev libx264-dev libgtk2.0-dev
+sudo apt-get install -y libv4l-dev v4l-utils
+sudo modprobe bcm2835-v4l2
+sudo apt-get install -y libatlas-base-dev gfortran
+
+# java
+sudo apt-get install -y openjdk-8-jre
+sudo apt-get install -y openjdk-11-jre
+
+# opencv
+sudo wget -qO - https://github.com/CJBuchel/CJ-Vision/blob/2.0/bootstrap/openCV.sh?raw=1 | bash
+
+
+# discord
