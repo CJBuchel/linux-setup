01bc87d2ff878e67583184f87f4542b35103d60a
Connor Buchel <cj@pop-os.localdomain>
2021-08-24 20:55:26 +0800

    whoops

--- linux-finalize.sh
+++ linux-finalize.sh
@@ -10,9 +10,9 @@ read -p "enter username e.g [cj]: " user
 # Load visual config
 echo "Setting flat-remix themes"
 
-sudo dconf load / < saved_settings.dconf
+dconf load / < saved_settings.dconf
 echo "Setting terminal theme"
-sudo cat custom_bashrc > /home/cj/.bashrc
+cat custom_bashrc > /home/$user/.bashrc
 
 # echo "Run `dconf load / < saved_settings.dconf` and `cat ./custom_bashrc > ~/.bashrc` to finish terminal setup"
 # echo "Setup complete, run `sudo howdy add {name}` to add user face"
